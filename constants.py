fe70be1f9c20e0308f162a4aa53e9e252e9c0c02
wild-trunk <forspedea@gmail.com>
2025-07-12 20:47:40 +0200

    Public release of version 0.0.2

---
+++ constants.py
@@ -0,0 +1,34 @@
+# messages
+TIMEOUT_ERROR = "Timeout error occured, experience might be degraded." 
+GENERAL_ERROR = "Unforseeable error occured: "
+
+OUTPUT_PATH_MANAGER_DEFAULT_VALUE_MESSAGE = "Output paths have been automatically generated, no need to select them."
+
+
+# globals
+DATETIME_PLACEHOLDER = 'current-datetime'
+
+TIMEOUT_TIME_IN_SECONDS = 15
+
+OUTPUT_FOLDER_WATCHER_POOLING_RATE_IN_SEC = 0.2
+
+SEMANTIC_DEFINITION_VALUE_FIELD = 'code'
+SEMANTIC_DEFINITION_LABEL_FIELD = 'name'
+SEMANTIC_DEFINITION_COLOR_FIELD = 'color'
+
+POSSIBLE_NODE_KEY_SCHEMA_ID = 'out_semantic_definition_schema_id'
+POSSIBLE_NODE_KEY_MODEL_ID  = 'ai_model_id'
+
+
+# system related
+SYSTEM_MACOS   = 'Darwin'
+SYSTEM_LINUX   = 'Linux'
+SYSTEM_WINDOWS = 'Windows'
+
+
+# GUI related
+DATA_DISPLAYING_BEHAVIOR_NON = 'off'
+DATA_DISPLAYING_BEHAVIOR_ON_FINISH = 'on-finish'
+DATA_DISPLAYING_BEHAVIOR_REALTIME = 'realtime'
+
+TEXT_SIZE_INSIDE_MSG_BOX = 11
